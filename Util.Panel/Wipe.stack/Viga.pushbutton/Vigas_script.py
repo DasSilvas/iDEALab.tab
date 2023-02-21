a9bcf2b5691b810ddb1e0ae6a88b682e02767b61
jf <joaoferreira191@gmail.com>
2023-02-21 22:07:11 +0000

    new file:   Util.Panel/Wipe.stack/Pilar.pushbutton/Icon.png 	new file:   Util.Panel/Wipe.stack/Pilar.pushbutton/Pilar_script.py 	new file:   Util.Panel/Wipe.stack/Pilar.pushbutton/classes.py 	new file:   Util.Panel/Wipe.stack/Sapata.pushbutton/Icon.png 	new file:   Util.Panel/Wipe.stack/Sapata.pushbutton/Sapata_script.py 	new file:   Util.Panel/Wipe.stack/Sapata.pushbutton/classes.py 	new file:   Util.Panel/Wipe.stack/Viga.pushbutton/Icon.png 	new file:   Util.Panel/Wipe.stack/Viga.pushbutton/Vigas_script.py 	new file:   Util.Panel/Wipe.stack/Viga.pushbutton/classes.py

---
+++ Util.Panel/Wipe.stack/Viga.pushbutton/Vigas_script.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Apaga os comentarios das vigas"""
+# Load the Python Standard and DesignScript Libraries
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript import Geometry as geom
+clr.AddReference("RevitNodes")
+import Revit
+from Revit import Elements
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+from classes import RvtApiCategory as cat
+
+doc = __revit__.ActiveUIDocument.Document
+
+collector = FilteredElementCollector(doc)
+filtro = ElementCategoryFilter(cat.VIGA)
+elements = collector.WherePasses(filtro).WhereElementIsNotElementType().ToElements()
+
+# Transcao com o REVIT para fazer as armaduras
+
+t = Transaction(doc, "Wipe Beam Comments")
+t.Start()
+
+for element in elements:
+    element.LookupParameter("Comments").Set("")
+
+t.Commit()
